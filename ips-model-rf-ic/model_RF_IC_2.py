33e8e7d5b2c5602f15ca4057f50fe0f9cf49b98a
Don  Batchelor <batchelordb@ornl.gov>
2016-03-03 12:13:57 -0800

    Added model-rf-ic

---
+++ ips-model-rf-ic/model_RF_IC_2.py
@@ -0,0 +1,114 @@
+#! /usr/bin/env python
+
+# version 0.0 3/28/08 (Batchelor)
+
+# ------------------------------------------------------------------------------
+#
+# RF_IC component script to drive change_ICRF_profiles executable. The executable requires 3
+# commandline arguments:
+# 1) current state file
+# 2) mode = one of "INIT", "STEP", "FINALIZE"
+# 3) timeStamp = initial time for "INIT", or = time at end of time stamp for "STEP"
+#
+# ------------------------------------------------------------------------------
+
+import sys
+import os
+import subprocess
+import getopt
+import shutil
+import string
+from component import Component
+
+class model_RF_IC_2 (Component):
+    def __init__(self, services, config):
+        Component.__init__(self, services, config)
+        print 'Created %s' % (self.__class__)
+
+# ------------------------------------------------------------------------------
+#
+# init function
+#
+# ------------------------------------------------------------------------------
+
+    def init(self, timeStamp=0):
+        print 'model_RF_IC.init() called'
+
+        services = self.services
+
+
+# Copy current state files and input files over to working directory
+        self.services.stage_plasma_state()
+        self.services.stage_input_files(self.INPUT_FILES)
+        cur_state_file = self.services.get_config_param('CURRENT_STATE')
+        cur_eqdsk_file = self.services.get_config_param('CURRENT_EQDSK')
+        cur_cql_file = self.services.get_config_param('CURRENT_CQL')
+        cur_dql_file = self.services.get_config_param('CURRENT_DQL')
+
+        RF_IC_bin = os.path.join(self.BIN_PATH, 'model_RF_IC_2_mcmd')
+
+        print 'Executing ', [RF_IC_bin, cur_state_file, 'INIT', timeStamp]
+        retcode = subprocess.call([RF_IC_bin, cur_state_file, cur_eqdsk_file,
+        cur_cql_file, cur_dql_file, 'INIT', timeStamp])
+        if (retcode != 0):
+            print 'Error executing ', RF_IC_bin
+            return 1
+# Update (original) plasma state
+        services.update_plasma_state()
+# "Archive" output files in history directory
+        services.stage_output_files(timeStamp, self.OUTPUT_FILES)
+        return 0
+
+# ------------------------------------------------------------------------------
+#
+# STEP function
+#
+# Does nothing out of the ordinary for a component script 'step' function
+#
+# ------------------------------------------------------------------------------
+
+    def step(self, timeStamp):
+        print 'model_RF_IC_2.step() called'
+
+        if (self.services == None) :
+            print 'Error in model_RF_IC:;step () : init() function not called before step().'
+            return 1
+        services = self.services
+
+# Copy current state files and input files over to working directory
+        self.services.stage_plasma_state()
+        self.services.stage_input_files(self.INPUT_FILES)
+        cur_state_file = self.services.get_config_param('CURRENT_STATE')
+        cur_eqdsk_file = self.services.get_config_param('CURRENT_EQDSK')
+        cur_cql_file = self.services.get_config_param('CURRENT_CQL')
+        cur_dql_file = self.services.get_config_param('CURRENT_DQL')
+        pwd = os.getcwd()
+
+# Call model_RF_IC
+        RF_IC_bin = os.path.join(self.BIN_PATH, 'model_RF_IC_2')
+
+        print 'Executing ', [RF_IC_bin, cur_state_file, 'STEP', timeStamp]
+        task_id = services.launch_task(self.NPROC, pwd, RF_IC_bin, cur_state_file,
+             cur_eqdsk_file, cur_cql_file, cur_dql_file, 'STEP', timeStamp)
+        retcode = services.wait_task(task_id)
+
+        if (retcode != 0):
+            print 'Error executing ', RF_IC_bin
+            return 1
+# Update plasma state
+        services.update_plasma_state()
+
+# "Archive" output files in history directory
+        services.stage_output_files(timeStamp, self.OUTPUT_FILES)
+        return 0
+# ------------------------------------------------------------------------------
+#
+# finalize function
+#
+# Does nothing
+# ------------------------------------------------------------------------------
+
+
+
+    def finalize(self, timestamp = 0):
+        print 'model_RF_IC finalize() called'
