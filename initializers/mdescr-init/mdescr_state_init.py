f28a75dbe24b8f3c5e35eae33ac1e4a826099bcd
Don  Batchelor <batchelordb@ornl.gov>
2016-03-31 11:55:12 -0700

    fixed  reference to BIN

--- initializers/mdescr-init/mdescr_state_init.py
+++ initializers/mdescr-init/mdescr_state_init.py
@@ -16,27 +16,27 @@
 # CURRENT_EQDSK, CURRENT_CQL, CURRENT_DQL, CURRENT_EQDSK].  We can always add more.
 #
 # This produces a CURRENT_STATE that in addition to the machine description data has:
-# time variables - ps%t0, ps%t1, ps%tinit, and ps%tfinal 
-# simulation identifiers - ps%tokamak_id, ps%shot_number, ps%run_id.  
+# time variables - ps%t0, ps%t1, ps%tinit, and ps%tfinal
+# simulation identifiers - ps%tokamak_id, ps%shot_number, ps%run_id.
 # ps%Global_label is set to run_id_tokamak_id_shot_number.
 #
 # This component drives the fortran executable mdescr_state_init.f90 which uses
 # Plasma State calls to generate CURRENT_STATE
 #
 # This version also supports RESTART as specified by the SIMULATION_MODE variable in
-# the config file.  For a restart run the plasma state files are retrieved 
-# by the framework from the path indicated by the INPUT_DIR config parameter in the 
+# the config file.  For a restart run the plasma state files are retrieved
+# by the framework from the path indicated by the INPUT_DIR config parameter in the
 # [mdescr_state_init] section.  The new values of ps%t0 and ps%tfinal are written into
 # CURRENT_STATE, and CURRENT_STATE is copied to PRIOR_STATE and NEXT_STATE if these are
 # in the PLASMA_STATE_FILES list.  The state files are copied to the plasma state work
 # directory by services.update_plasma_state().
 #
-# Nota Bene: For restart the plasma state files should be listed in the config file as  
+# Nota Bene: For restart the plasma state files should be listed in the config file as
 # input files to the mdescr_state_init component.
 #
 # N.B. Both ps%t0 and ps%t1 are set to the value time_stamp.  tinit and tfinal
 #      are generated here from the TIME_LOOP variable in the
-#      simulation config file.  Note that the initial t0 can be different from 
+#      simulation config file.  Note that the initial t0 can be different from
 #      tinit, as might be needed in a restart.
 #
 # ------------------------------------------------------------------------------
@@ -108,7 +108,7 @@ class mdescr_state_init (Component):
 # RESTART simulation mode
 #
 # ------------------------------------------------------------------------------
-            
+
         if mode == 'RESTART':
             # Get restart files listed in config file. Here just the plasma state files.
             try:
@@ -118,9 +118,9 @@ class mdescr_state_init (Component):
             except Exception, e:
                 print 'Error in call to get_restsrt_files()' , e
                 raise
-            
+
             cur_state_file = self.services.get_config_param('CURRENT_STATE')
-    
+
             # Update ps%t0, ps%t1 and ps%tfinal. Note ps%tinit stays the same in the plasma
             # state file, but this value of tinit is the restart time from the config file
             ps = NetCDFFile(cur_state_file, 'r+')
@@ -128,41 +128,39 @@ class mdescr_state_init (Component):
             ps.variables['t1'].assignValue(float(tinit))
             ps.variables['tfinal'].assignValue(float(tfinal))
             ps.close()
-        
+
 # ------------------------------------------------------------------------------
 #
 # NORMAL simulation mode
 #
 # ------------------------------------------------------------------------------
-        
+
         else:
-        
+
             # Get run identifiers
             shot_number = services.get_config_param('SHOT_NUMBER')
             run_id = services.get_config_param('RUN_ID')
-            
-            mdescr_file = self.MDESCR_FILE            
+
+            mdescr_file = self.MDESCR_FILE
             print 'mdescr_file =', mdescr_file, '  shot_number = ', shot_number,
             '   run_id = ', run_id
-    
+
             # Generate initial plasma state files
             services.stage_input_files(self.INPUT_FILES)
             cur_state_file = services.get_config_param('CURRENT_STATE')
+
+            init_bin = os.path.join(self.BIN_PATH, 'mdescr_state_init')
     
-            #init_bin = os.path.join(self.BIN_PATH, 'mdescr_state_init')
-            init_bin = self.BIN
-   
             print 'Executing ', [init_bin, cur_state_file]
-            retcode = subprocess.call([init_bin, cur_state_file, 
+            retcode = subprocess.call([init_bin, cur_state_file,
                 mdescr_file, shot_number, run_id, tinit, tfinal, t])
             if (retcode != 0):
-                logMsg = 'Error executing ' + init_bin
-                self.services.error(logMsg)
-                raise Exception(logMsg)
+               print 'Error executing ', init_bin
+               raise
                
-            # Put in any shot configuration data the components need in order 
+            # Put in any shot configuration data the components need in order
             # to initialize themselves.
-            
+
 # The only component now that needs this is NUBEAM.  It expects TSC to have specified the
 # beam species labels (H,D, T ...).  What is here is a quick and dirty way to get the
 # 2 ITER beams set to 'D'.  This should be put into the NUBEAM section of the config file.
@@ -170,26 +168,26 @@ class mdescr_state_init (Component):
 #             ps.variables['ps%nbion'][0] = 'D'
 #             ps.variables['ps%nbion'][1] = 'D'
 #             ps.close()
-               
+
             # Generate other state files
             try:
                 cur_eqdsk_file = services.get_config_param('CURRENT_EQDSK')
                 subprocess.call(['touch', cur_eqdsk_file])
             except Exception, e:
                 print 'No CURRENT_EQDSK file ', e
-    
+
             try:
                 cur_cql_file = services.get_config_param('CURRENT_CQL')
                 subprocess.call(['touch', cur_cql_file])
             except Exception, e:
                 print 'No CURRENT_CQL file ', e
-    
+
             try:
                 cur_dql_file = services.get_config_param('CURRENT_DQL')
                 subprocess.call(['touch', cur_dql_file])
             except Exception, e:
                 print 'No CURRENT_DQL file ', e
-    
+
             try:
                 cur_jsdsk_file = services.get_config_param('CURRENT_JSDSK')
                 subprocess.call(['touch', cur_jsdsk_file])
@@ -232,11 +230,11 @@ class mdescr_state_init (Component):
 
     def checkpoint(self, timestamp=0.0):
         print 'mdescr_state_init.checkpoint() called'
-        
+
         services = self.services
         services.stage_plasma_state()
         services.save_restart_files(timestamp, self.RESTART_FILES)
-        
+
 
 # ------------------------------------------------------------------------------
 #
@@ -249,5 +247,3 @@ class mdescr_state_init (Component):
 
     def finalize(self, timestamp=0.0):
         print 'mdescr_state_init.finalize() called'
-
-
