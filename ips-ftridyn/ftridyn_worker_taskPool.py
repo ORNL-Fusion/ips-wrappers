cabff663c8627dc84bcc0f15eacb1eaf597b3faf
Timothy Younkin <tyounkin@vols.utk.edu>
2018-03-18 15:14:27 -0700

    added ips-nested_ftridyn_gitr_xolotl wrappers from examples and updated ftridyn component to use mpi4py

--- ips-ftridyn/ftridyn_worker_taskPool.py
+++ ips-ftridyn/ftridyn_worker_taskPool.py
@@ -10,6 +10,8 @@ import generate_ftridyn_input
 import analyze_ftridyn_simulations
 import numpy as np
 import netCDF4
+import pickle
+import math
 
 class ftridynWorker(Component):
     def __init__(self, services, config):
@@ -48,6 +50,7 @@ class ftridynWorker(Component):
                         shutil.copyfile(self.INPUT_DIR+"/dist"+str(int(j))+".dat", pathString+"/"+ffilename+"0001.ED1") 
  
     def step(self, timeStamp=0.0,**keywords):
+        start_time = time.time()
         print('ftridyn_worker: step (task pool version)')
         
         ffilename = keywords["ffilename"]
@@ -57,6 +60,7 @@ class ftridynWorker(Component):
         energy = keywords["eArg"]
         angle = keywords["aArg"]
         roughness = keywords["dArg"]
+        file_namelist = []
 
         nEgrid = 100
         maxE = 100.0
@@ -73,48 +77,111 @@ class ftridynWorker(Component):
         cosZDistributionRef = np.zeros(shape=(len(energy),len(angle),nAgrid)) 
         eDistribution = np.zeros(shape=(len(energy),len(angle),nEgrid)) 
         eDistributionRef = np.zeros(shape=(len(energy),len(angle),nEgrid)) 
+        
+        nFTruns = len(file_namelist)
+        nFTrunsPerNode = int(self.FTMPI_PPN)
+        nFTrunNodes = int(self.FTMPI_NODES)
+        if(nFTruns <= nFTrunsPerNode*nFTrunNodes): 
+            nFTrunsPerNode = int(math.ceil(1.0*nFTruns/nFTrunNodes))
+            nFTpoolTasks = nFTrunNodes
+        else:
+            nFTpoolTasks = int(math.ceil(1.0*nFTruns/nFTrunsPerNode))
+        
         cwd = self.services.get_working_dir()
-        pool = self.services.create_task_pool('pool')
+        #pool = self.services.create_task_pool('pool')
         for i in range(len(energy)):
             for j in range(len(angle)):
                 for k in range(len(roughness)):
                     pathString = "FTRIDYN_"+str(energy[i]) + "_"+str(angle[j])+"_" + str(roughness[k])
-                    self.services.add_task('pool', 'task'+str(energy[i]) + "_"+str(angle[j])+"_" + str(roughness[k]), 1, cwd+"/"+pathString, self.FTRIDYN_EXE, ffilename+"0001.IN",logfile='task_'+pathString+'.log' )
+                    file_namelist.append(pathString)
+                    #self.services.add_task('pool', 'task'+str(energy[i]) + "_"+str(angle[j])+"_" + str(roughness[k]), 1, cwd+"/"+pathString, self.FTRIDYN_EXE, ffilename+"0001.IN",logfile='task_'+pathString+'.log' )
+        #ret_val = self.services.submit_tasks('pool')
+        #print 'ret_val = ', ret_val
+        #exit_status = self.services.get_finished_tasks('pool')
+        #print exit_status
+        #self.services.remove_task_pool('pool')
+        nFTruns = len(file_namelist)
+        nFTrunsPerNode = int(self.FTMPI_PPN)
+        nFTrunNodes = int(self.FTMPI_NODES)
+        if(nFTruns <= nFTrunsPerNode*nFTrunNodes): 
+            nFTrunsPerNode = int(math.ceil(1.0*nFTruns/nFTrunNodes))
+            nFTpoolTasks = nFTrunNodes
+        else:
+            nFTpoolTasks = int(math.ceil(1.0*nFTruns/nFTrunsPerNode))
+
+        with open('ftridyn_file_namelist.pkl', 'wb') as f:
+            pickle.dump(list(reversed(file_namelist)), f)
+
+        pool = self.services.create_task_pool('pool')
+        for i in range(nFTpoolTasks):
+            self.services.add_task('pool', 'task'+str(i), nFTrunsPerNode, cwd, 'python',self.FTMPI_EXEC,str(i), str(nFTrunsPerNode),task_ppn= nFTrunsPerNode,logfile='task_pool'+str(i)+'.log' )
         ret_val = self.services.submit_tasks('pool')
         print 'ret_val = ', ret_val
         exit_status = self.services.get_finished_tasks('pool')
         print exit_status
         self.services.remove_task_pool('pool')
 
+        exec_time = time.time()
+        print("Execution of FTRIDYN Cases took --- %s seconds ---" % (exec_time - start_time))
+        pool_py = self.services.create_task_pool('pool_py')
+        for i in range(nFTpoolTasks):
+            self.services.add_task('pool_py', 'task'+str(i), nFTrunsPerNode, cwd, 'python',self.FTMPI,str(i), str(nFTrunsPerNode),task_ppn= nFTrunsPerNode,logfile='task_'+str(i)+'.log' )
+        ret_val = self.services.submit_tasks('pool_py')
+        print 'ret_val = ', ret_val
+        exit_status = self.services.get_finished_tasks('pool_py')
+        print exit_status
+        self.services.remove_task_pool('pool_py')
+
+        #task_id = self.services.launch_task(self.FTMPI_TASKS,self.services.get_working_dir(),'python',self.FTMPI,task_ppn=self.FTMPI_PPN)
+        ##monitor task until complete
+        #if (self.services.wait_task(task_id)):
+        #    self.services.error('ftmpicomp: step failed.')
 
         spyl_file = ffilename+'SPYL.DAT'
         #driver_out = self.services.get_config_param('EA_OUTPUT')
         #fid = open(driver_out,'a')
+        #for i in range(len(file_namelist)):
+        totalIndex=0
         for i in range(len(energy)):
             for j in range(len(angle)):
-                for k in range(len(roughness)):
-                    pathString = "FTRIDYN_"+str(energy[i]) + "_"+str(angle[j])+"_" + str(roughness[k])
-                    print('doing analysis on ' , pathString+"/"+spyl_file)
-                    WW=analyze_ftridyn_simulations.ftridyn_output_data(pathString+"/"+ffilename,'0001')
-                    thisSpyl = WW.calculate_total_sputtering_yield()
-                    print('sputtering yield', thisSpyl)
-                    sputt[i,j] = thisSpyl
-                    thisRefyl = WW.calculate_total_reflection_yield()
-                    print('reflection yield', thisRefyl)
-                    refl[i,j] = thisRefyl
-                    nP, nX, binsX,nY,binsY,nZ, binsZ = analyze_ftridyn_simulations.plot_sputtered_angular_distributions(pathString+"/"+ffilename,nAgrid)
-                    cosXDistribution[i,j,:] = nX
-                    cosYDistribution[i,j,:] = nY
-                    cosZDistribution[i,j,:] = nZ
-                    nP, nX, binsX,nY,binsY,nZ, binsZ = analyze_ftridyn_simulations.plot_reflected_angular_distributions(pathString+"/"+ffilename,nAgrid)
-                    cosXDistributionRef[i,j,:] = nX
-                    cosYDistributionRef[i,j,:] = nY
-                    cosZDistributionRef[i,j,:] = nZ
-                    nPenergy, nenergy, binsenergy = analyze_ftridyn_simulations.plot_sputtered_energy_distributions(pathString+"/"+ffilename,nEgrid)
-                    eDistribution[i,j,:] = nenergy
-                    analyze_ftridyn_simulations.plot_implantation_profile(pathString+"/"+ffilename)
-                    nPenergyRef, nenergyRef, binsenergyRef = analyze_ftridyn_simulations.plot_reflected_energy_distributions(pathString+"/"+ffilename,nEgrid)
-                    eDistributionRef[i,j,:] = nenergyRef
+                pathString = file_namelist[totalIndex]
+                yr = np.loadtxt(pathString+"/"+"YR.out", dtype='float')
+                sputt[i,j] = yr[0]
+                refl[i,j] = yr[1]
+                nX = np.loadtxt(pathString+"/"+"nX.out", dtype='float')
+                cosXDistribution[i,j,:] = nX
+                nXref = np.loadtxt(pathString+"/"+"nXref.out", dtype='float')
+                cosXDistributionRef[i,j,:] = nXref
+                nenergy = np.loadtxt(pathString+"/"+"energy.out", dtype='float')
+                eDistribution[i,j,:] = nenergy
+                nenergyRef = np.loadtxt(pathString+"/"+"energyRef.out", dtype='float')
+                eDistributionRef[i,j,:] = nenergyRef
+                totalIndex = totalIndex+1
+        #for i in range(len(energy)):
+        #    for j in range(len(angle)):
+        #        for k in range(len(roughness)):
+        #            pathString = "FTRIDYN_"+str(energy[i]) + "_"+str(angle[j])+"_" + str(roughness[k])
+        #            print('doing analysis on ' , pathString+"/"+spyl_file)
+        #            WW=analyze_ftridyn_simulations.ftridyn_output_data(pathString+"/"+ffilename,'0001')
+        #            thisSpyl = WW.calculate_total_sputtering_yield()
+        #            print('sputtering yield', thisSpyl)
+        #            sputt[i,j] = thisSpyl
+        #            thisRefyl = WW.calculate_total_reflection_yield()
+        #            print('reflection yield', thisRefyl)
+        #            refl[i,j] = thisRefyl
+        #            nP, nX, binsX,nY,binsY,nZ, binsZ = analyze_ftridyn_simulations.plot_sputtered_angular_distributions(pathString+"/"+ffilename,nAgrid)
+        #            cosXDistribution[i,j,:] = nX
+        #            cosYDistribution[i,j,:] = nY
+        #            cosZDistribution[i,j,:] = nZ
+        #            nP, nX, binsX,nY,binsY,nZ, binsZ = analyze_ftridyn_simulations.plot_reflected_angular_distributions(pathString+"/"+ffilename,nAgrid)
+        #            cosXDistributionRef[i,j,:] = nX
+        #            cosYDistributionRef[i,j,:] = nY
+        #            cosZDistributionRef[i,j,:] = nZ
+        #            nPenergy, nenergy, binsenergy = analyze_ftridyn_simulations.plot_sputtered_energy_distributions(pathString+"/"+ffilename,nEgrid)
+        #            eDistribution[i,j,:] = nenergy
+        #            analyze_ftridyn_simulations.plot_implantation_profile(pathString+"/"+ffilename)
+        #            nPenergyRef, nenergyRef, binsenergyRef = analyze_ftridyn_simulations.plot_reflected_energy_distributions(pathString+"/"+ffilename,nEgrid)
+        #            eDistributionRef[i,j,:] = nenergyRef
 
                     #fid.write(" ".join([str(energy[i]),str(angle[j]),str(roughness[k]),'  ',str(thisSpyl),'\n']))
         #fid.close()
@@ -152,6 +219,8 @@ class ftridynWorker(Component):
         edist[:] = eDistribution
         edistref[:] = eDistributionRef
         rootgrp.close()
+        post_time = time.time()
+        print("Processing of FTRIDYN Cases took --- %s seconds ---" % (post_time - exec_time))
         #updates plasma state FTridyn output files
         self.services.update_plasma_state()
   
--- ips-gitr/gitr_comp.py
+++ ips-gitr/gitr_comp.py
@@ -24,7 +24,7 @@ class gitr_comp(Component):
         shutil.copyfile('ftridyn.nc','input/ftridyn.nc')
         task_id = self.services.launch_task(self.NPROC,
                                             self.services.get_working_dir(),
-                                            self.GITR_EXE,task_ppn=1,ppn=1)
+                                            self.GITR_EXE,task_ppn=self.TASK_PPN)
         #monitor task until complete
         if (self.services.wait_task(task_id)):
             self.services.error('gitr_comp: step failed.')
