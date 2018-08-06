a3e1763653a04af532955bde2a8a8500e7db51e1
Mark Cianciosa <cianciosamr@ornl.gov>
2018-08-06 17:07:05 -0400

    Add singular value decomposition analysis. Also fix test case so signal sigmas are positive.

--- ips-quasi-newton/quasi_newton_driver.py
+++ ips-quasi-newton/quasi_newton_driver.py
@@ -13,6 +13,7 @@ import os
 import shutil
 import json
 import numpy
+import math
 
 #-------------------------------------------------------------------------------
 #
@@ -47,11 +48,38 @@ class quasi_newton_driver(Component):
 #  Load the quasi-newton json file.
         with open(self.quasi_newton_config_file, 'r') as config_file:
             quasi_newton_config = json.load(config_file)
-            self.signal_sigma = numpy.array(quasi_newton_config['signal_sigma'])
+            self.signal_sigma = numpy.absolute(numpy.array(quasi_newton_config['signal_sigma']))
             self.signal_observed = numpy.array(quasi_newton_config['signal_observed'])
             self.signal_weights = numpy.sqrt(numpy.array(quasi_newton_config['signal_weights']))
             self.dchi2_tol = quasi_newton_config['dchi2_tol']
-
+        
+#  Singular value step controls.
+#  Cutoff value for relative singular values.
+            if 'cut_svd' in quasi_newton_config:
+                self.cut_svd = quasi_newton_config['cut_svd']
+            else:
+                self.cut_svd = 0.0
+#  Cutoff value for expected step efficiency.
+            if 'cut_eff' in quasi_newton_config:
+                self.cut_eff = quasi_newton_config['cut_eff']
+            else:
+                self.cut_eff = 0.0
+#  Cutoff value for expected marginal step efficiency.
+            if 'cut_marg_eff' in quasi_newton_config:
+                self.cut_marg_eff = quasi_newton_config['cut_marg_eff']
+            else:
+                self.cut_marg_eff = 0.0
+#  Cutoff value for expected step size.
+            if 'cut_delta_a' in quasi_newton_config:
+                self.cut_delta_a = quasi_newton_config['cut_delta_a']
+            else:
+                self.cut_delta_a = 0.0
+#  Cutoff value for expected change in g^2.
+            if 'cut_dg2' in quasi_newton_config:
+                self.cut_dg2 = quasi_newton_config['cut_dg2']
+            else:
+                self.cut_dg2 = 0.0
+            
 #  Set keys for the subworkflows.
             keys = {'PWD'              : self.services.get_config_param('PWD'),
                     'USER_INPUT_FILES' : self.current_model_state}
@@ -110,9 +138,6 @@ class quasi_newton_driver(Component):
                 zip_ref.extract(self.model_workers[0]['result'])
                 with open(self.model_workers[0]['result'], 'r') as result_file:
                     self.signal_model = numpy.array(json.load(result_file)['signal_model'])
-                
-#  FIXME: DEBUGING REMOVE
-                os.rename(self.model_workers[0]['result'], 'saved.json')
 
 #  The initial model state may have changed reset it from the one of the
 #  workers.
@@ -128,144 +153,24 @@ class quasi_newton_driver(Component):
 
 #  Compute chi^2. Set the inital change in chi^2 higher than the tolarance to
 #  ensure at least one iteration of the while loop is performed.
-        e = self.signal_weights*((self.signal_model - self.signal_observed)/self.signal_sigma)
-        chi2 = numpy.sum(e*e)
+        self.e = self.signal_weights*((self.signal_model - self.signal_observed)/self.signal_sigma)
+        chi2 = numpy.dot(self.e, self.e)
         dchi2 = self.dchi2_tol + 1.0
 
-        jacobian = numpy.empty((len(self.model_workers), e.size), dtype=float)
-#        hessian = numpy.empty((len(self.model_workers), len(self.model_workers)), dtype=float)
-#        gradient = numpy.empty(len(self.model_workers), dtype=float)
-        etry = numpy.empty((len(self.model_workers), e.size), dtype=float)
+        self.jacobian = numpy.empty((len(self.model_workers), self.e.size), dtype=float)
+        self.hessian = numpy.empty((len(self.model_workers), len(self.model_workers)), dtype=float)
+        self.gradient = numpy.empty(len(self.model_workers), dtype=float)
+        self.delta_a = numpy.empty((min(len(self.e), len(self.model_workers)) + 1, len(self.model_workers)), dtype=float)
+
+        etry = numpy.empty((len(self.model_workers), self.e.size), dtype=float)
         chi2try = numpy.empty(len(self.model_workers), dtype=float)
 
 #  Perform a quasi-newton minimization.
         while dchi2 > self.dchi2_tol:
             timeStamp += 1.0
-    
-#  Set the model to a known state.
-            shutil.copy2(self.current_model_state, 'model_inputs')
-            for worker in self.model_workers:
-                worker['wait'] = self.services.call_nonblocking(worker['init'],
-                                                                'init', timeStamp)
-
-#  Perturb the parameters.
-            for worker in self.model_workers:
-                self.services.wait_call(worker['wait'], True)
-                keywords = {worker['name'] : worker['value'] + worker['vrnc']}
-                print(keywords[worker['name']])
-                worker['wait'] = self.services.call_nonblocking(worker['driver'],
-                                                                'init', timeStamp,
-                                                                **keywords)
             
-#  Recompute the model.
-            for worker in self.model_workers:
-                self.services.wait_call(worker['wait'], True)
-                worker['wait'] = self.services.call_nonblocking(worker['driver'], 'step', timeStamp,
-                                                                result_file=worker['result'])
-
-#  Collect the results and reset the model.
-            for worker in self.model_workers:
-                self.services.wait_call(worker['wait'], True)
-                worker['wait'] = self.services.call_nonblocking(worker['init'],
-                                                                'init', timeStamp)
-                        
-            self.services.stage_subflow_output_files()
-
-#  Compute the normalized jacobian A.
-#
-#    A_ij = d e_i/d a_j                                                      (1)
-#
-#  Where e is the error vector.
-#
-#    e_i = W_i*((S_i - M_i)/sigma_i)^2                                       (2)
-#
-#  Note due to the what the memory is laid out the Jacobian is transposed.
-            for i, worker in enumerate(self.model_workers):
-                with ZipState.ZipState(worker['output'], 'a') as zip_ref:
-                    zip_ref.extract(worker['result'])
-                    with open(worker['result'], 'r') as result_file:
-                        self.signal_model = numpy.array(json.load(result_file)['signal_model'])
-
-                ep = self.signal_weights*((self.signal_model - self.signal_observed)/self.signal_sigma)
-                jacobian[i] = e - ep
-#  Approximate the hessian.
-#
-#    alpha = A^T * A
-#
-#  The gradient
-#
-#    beta = A^T * e
-#
-#            hessian = numpy.dot(jacobian, numpy.transpose(jacobian))
-#            gradient = numpy.dot(jacobian, e)
-#            delta_a = gradient*numpy.dot(gradient, gradient)
-#            delta_a /= numpy.dot(gradient, numpy.dot(hessian, gradient))
-            j_svd_u, j_svd_w, j_svd_vt = numpy.linalg.svd(numpy.transpose(jacobian))
-
-            print(numpy.transpose(jacobian))
-            print('------------------------------------------------------------')
-            print(j_svd_u)
-            print('------------------------------------------------------------')
-            print(j_svd_w)
-            print('------------------------------------------------------------')
-            print(j_svd_vt)
-
-            delta_a = numpy.empty((len(j_svd_w), len(self.model_workers)), dtype=float)
-            
-            temp_work1 = numpy.where(j_svd_w > 0, 1.0/j_svd_w, 0)
-            temp_work2 = numpy.dot(numpy.transpose(j_svd_u), e)
-            temp_work3 = numpy.zeros(len(self.model_workers))
-            for i in range(len(j_svd_w)):
-                temp_work3[i] = temp_work1[i]*temp_work2[i]
-                delta_a[i] = numpy.dot(numpy.transpose(j_svd_vt), temp_work3)
-
-            new_values = []
-            for i, worker in enumerate(self.model_workers):
-                new_values.append(delta_a[i]*worker['vrnc'])
-
-            for worker in self.model_workers:
-                keywords = {}
-                for new_value in new_values:
-                    keywords[worker['name']] = worker['value'] + new_value*(worker['scale']*50.0 + 50.0)
-                self.services.wait_call(worker['wait'], True)
-                worker['wait'] = self.services.call_nonblocking(worker['driver'],
-                                                                'init', timeStamp,
-                                                                **keywords)
-
-            for worker in self.model_workers:
-                self.services.wait_call(worker['wait'], True)
-                worker['wait'] = self.services.call_nonblocking(worker['driver'], 'step', timeStamp,
-                                                                result_file=worker['result'])
-
-#  Collect the results and reset the model.
-            for worker in self.model_workers:
-                self.services.wait_call(worker['wait'], True)
-            self.services.stage_subflow_output_files()
-
-            for i, worker in enumerate(self.model_workers):
-                with ZipState.ZipState(worker['output'], 'a') as zip_ref:
-                    zip_ref.extract(worker['result'])
-                    with open(worker['result'], 'r') as result_file:
-                        self.signal_model = numpy.array(json.load(result_file)['signal_model'])
-            
-                etry[i] = self.signal_weights*((self.signal_observed - self.signal_model)/self.signal_sigma)
-                chi2try[i] = numpy.dot(etry[i], etry[i])
-
-            min_i = numpy.argmin(chi2try)
-            dchi2 = chi2 - chi2try[min_i]
-            chi2 = chi2try[min_i]
-
-            for i, worker in enumerate(self.model_workers):
-                worker['value'] += new_values[i]*(self.model_workers[min_i]['scale']*50.0 + 50.0)
-
-            os.rename(self.model_workers[min_i]['output'], self.current_model_state)
-            print('--------------------------------------------------------------------------------')
-            print('--------------------------------------------------------------------------------')
-            print('--------------------------------------------------------------------------------')
-            print(timeStamp, min_i, chi2, dchi2)
-            print('--------------------------------------------------------------------------------')
-            print('--------------------------------------------------------------------------------')
-            print('--------------------------------------------------------------------------------')
+            self.eval_jacobian(timeStamp)
+            num_sv = self.get_k_svd()
 
 #  FIXME: Force the loop to end.
             dchi2 = 0
@@ -287,3 +192,159 @@ class quasi_newton_driver(Component):
         for worker in self.model_workers:
             self.services.wait_call_list(worker['wait'], True)
 
+#-------------------------------------------------------------------------------
+#
+#  QUASI-NEWTON Driver eval_jacobian method. This perturbs each parameter
+#  independently in parallel.
+#
+#-------------------------------------------------------------------------------
+    def eval_jacobian(self, timeStamp=0.0):
+        print('quasi_newton_driver: eval_jacobian')
+
+#  Set the model to a known state.
+        shutil.copy2(self.current_model_state, 'model_inputs')
+        for worker in self.model_workers:
+            worker['wait'] = self.services.call_nonblocking(worker['init'],
+                                                            'init', timeStamp)
+                
+#  Perturb the parameters.
+        for worker in self.model_workers:
+            self.services.wait_call(worker['wait'], True)
+            keywords = {worker['name'] : worker['value'] + worker['vrnc']}
+            worker['wait'] = self.services.call_nonblocking(worker['driver'],
+                                                            'init', timeStamp,
+                                                            **keywords)
+                                                            
+#  Recompute the model.
+        for worker in self.model_workers:
+            self.services.wait_call(worker['wait'], True)
+            worker['wait'] = self.services.call_nonblocking(worker['driver'], 'step', timeStamp,
+                                                            result_file=worker['result'])
+                                                                    
+#  Collect the results and reset the model.
+        for worker in self.model_workers:
+            self.services.wait_call(worker['wait'], True)
+            worker['wait'] = self.services.call_nonblocking(worker['init'],
+                                                            'init', timeStamp)
+                                                                            
+        self.services.stage_subflow_output_files()
+                                                                            
+#  Compute the normalized jacobian A.
+#
+#    A_ij = d e_i/d a_j                                                      (1)
+#
+#  Where e is the error vector.
+#
+#    e_i = W_i*((S_i - M_i)/sigma_i)^2                                       (2)
+#
+#  Note due to the what the memory is laid out the Jacobian is transposed.
+        for i, worker in enumerate(self.model_workers):
+            with ZipState.ZipState(worker['output'], 'a') as zip_ref:
+                zip_ref.extract(worker['result'])
+                with open(worker['result'], 'r') as result_file:
+                    self.signal_model = numpy.array(json.load(result_file)['signal_model'])
+                                                                                            
+            self.jacobian[i] = self.e - self.signal_weights*((self.signal_model - self.signal_observed)/self.signal_sigma)
+
+#-------------------------------------------------------------------------------
+#
+#  QUASI-NEWTON Driver get_k_svd method. Performs a singular value decomposition
+#  of the jacobian. Based on various cuttoffs, the maximum number of sigular
+#  values retained is determined.
+#
+#-------------------------------------------------------------------------------
+    def get_k_svd(self):
+        print('quasi_newton_driver: get_k_svd')
+
+#  Approximate the hessian.
+#
+#    alpha = A^T * A                                                         (1)
+#
+#  The gradient
+#
+#    beta = A^T * e                                                          (2)
+#
+        self.hessian = numpy.matmul(self.jacobian, numpy.transpose(self.jacobian))
+        self.gradient = numpy.dot(self.jacobian, self.e)
+        
+#  Compute the steepest descent setp size in normalized parameter space.
+#
+#    da = beta * beta * beta / (beta * alpha * beta)                         (3)
+        self.delta_a[0,:] = self.gradient*numpy.dot(self.gradient, self.gradient)/numpy.dot(self.gradient, numpy.matmul(self.hessian, self.gradient))
+
+#  Singular value decomposition of the Jacobian.
+        j_svd_u, j_svd_w, j_svd_vt = numpy.linalg.svd(numpy.transpose(self.jacobian))
+        
+#  Define Inverse singular values.
+        temp_work1 = numpy.where(j_svd_w > 0, 1.0/j_svd_w, 0)
+#    U^T * e                                                                 (4)
+        temp_work2 = numpy.dot(numpy.transpose(j_svd_u), self.e)
+
+#  Perform pseudo-inversion for successive numbers of singular values retained.
+        temp_work3 = numpy.zeros(len(self.model_workers))
+        for i in range(0, len(j_svd_w)):
+            temp_work3[i] = temp_work1[i]*temp_work2[i]
+            self.delta_a[i + 1,:] = numpy.matmul(numpy.transpose(j_svd_vt), temp_work3)
+
+#  Estimate the expected changes in g^2. Equation 22 in Hanson et. al.
+#  doi: 10.1088/0029-5515/49/7/075031
+        exp_dg2 = numpy.empty(len(j_svd_w) + 1, dtype=float)
+        delta_a_len = numpy.empty(len(j_svd_w) + 1, dtype=float)
+        exp_eff = numpy.empty(len(j_svd_w) + 1, dtype=float)
+        for i in range(0, len(exp_dg2)):
+            exp_dg2[i] = self.get_exp_dg2(self.delta_a[i,:])
+            delta_a_len[i] = math.sqrt(numpy.dot(self.delta_a[i,:], self.delta_a[i,:]))
+            
+#  Equation 23 in Hanson et. al. doi: 10.1088/0029-5515/49/7/075031
+            exp_eff[i] = abs(exp_dg2[i])/delta_a_len[i]
+
+
+#  Although marginal efficiencies are not strictly defined for the Steepest
+#  Descent (index 0) and just one singular value cases, for convenience define
+#  them here.
+        marg_exp_eff = numpy.empty(len(j_svd_w) + 1, dtype=float)
+        marg_exp_eff[0:1] = exp_eff[0:1]
+        for i in range(2, len(exp_dg2)):
+            d_len = max(delta_a_len[i] - delta_a_len[i - 1], 1.0e-10)
+            marg_exp_eff[i] = abs(exp_dg2[i]) - abs(exp_dg2[i - 1])/d_len
+    
+#  Check for cutoffs.
+        largest_w = 1.0
+        if j_svd_w[0] > 0:
+            largest_w = j_svd_w[0]
+
+#  Find the largest number of singular values to use.
+        for i in range(len(j_svd_w), 1, -1):
+            meets_cut = (j_svd_w[i - 1]/largest_w >= self.cut_svd)
+            meets_cut = meets_cut and (exp_eff[i] >= self.cut_eff)
+            meets_cut = meets_cut and (marg_exp_eff[i] >= self.cut_marg_eff)
+            meets_cut = meets_cut and (delta_a_len[i] >= self.cut_delta_a)
+            meets_cut = meets_cut and (numpy.abs(exp_dg2[i]) >= self.cut_dg2)
+            if meets_cut:
+                return i
+
+#  All the selected criteria failed use no singular values.
+        return 0
+        
+#-------------------------------------------------------------------------------
+#
+#  QUASI-NEWTON Driver get_exp_dg2 method. Obtains the expected change in g^2.
+#
+#-------------------------------------------------------------------------------
+    def get_exp_dg2(self, delta):
+        print('quasi_newton_driver: get_exp_dg2')
+        
+#  Linear part.
+#
+#    2e * A * da                                                             (1)
+        exp_dg2_lin = 2.0*numpy.dot(self.e, numpy.matmul(numpy.transpose(self.jacobian), delta))
+
+#  Qaudratic part.
+#
+#    da * A^T * A * da = da * alpha * da                                     (2)
+#
+#  Alpha is the hessian matrix. Equation 14 in Hanson et. al.
+#  doi: 10.1088/0029-5515/49/7/075031
+        exp_dg2_quad = numpy.dot(delta, numpy.matmul(self.hessian, delta))
+
+        return exp_dg2_lin - exp_dg2_quad
