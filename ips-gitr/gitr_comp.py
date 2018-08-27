108e494087caffd6a92c735fe8d6fe5623ae64a3
Timothy Younkin <tyounkin@vols.utk.edu>
2018-08-27 08:52:12 -0700

    updates to gitr_comp for mq4

--- ips-gitr/gitr_comp.py
+++ ips-gitr/gitr_comp.py
@@ -5,6 +5,8 @@ import gitr
 import os
 import pickle
 import shutil
+import gitrParticleSource
+import hpic
 
 class gitr_comp(Component):
     def __init__(self, services, config):
@@ -27,6 +29,12 @@ class gitr_comp(Component):
             shutil.copyfile('ftridynSelf.nc','input/ftridynSelf.nc')
 	if os.path.exists('profiles.nc'):
             shutil.copyfile('profiles.nc','input/profiles.nc')
+        hpic.plot_hpic_ieadDavide(solps_path=str(self.HPIC_SOLPS_FILE),HpicDataFolder=str(self.HPIC_DATA_DIR))
+        hpic.computeSputtYld()
+        bgFlux,flux_fracs = hpic.printBackgroundDist()
+        fluxPerParticle = gitrParticleSource.particleSource(geomFile=str(self.GEOM_FILE),nParticles=int(self.NP)) 
+	if os.path.exists('particleSource.nc'):
+            shutil.copyfile('particleSource.nc','input/particleSource.nc')
         task_id = self.services.launch_task(self.NPROC,
                                             self.services.get_working_dir(),
                                             self.GITR_EXE,task_ppn=self.TASK_PPN,
@@ -37,10 +45,10 @@ class gitr_comp(Component):
         
         #gitr.piscesProcessing(path=self.BASE_DIR)
 	#gitr.iter2dProcessing()
-	nLocations = gitr.iter3dProcessing()
+	nLocations = gitr.iter3dProcessingQ4(nParticles=int(self.NP),totalParticleRate=fluxPerParticle,path = '',locRmRs=[-0.1,0.02,0.09,0.2],flux_fracs =flux_fracs,locWidth = 0.02)
         self.services.update_plasma_state()         
         for i in range(nLocations):
-            shutil.copyfile('gitrOut_'+str(i)+'.txt',self.BASE_DIR+'/gitrOut_'+str(i)+'.txt')
+            shutil.copyfile('gitrOut'+str(i)+'.txt',self.BASE_DIR+'/gitrOut'+str(i)+'.txt')
         return
     
     def finalize(self, timeStamp=0.0):
