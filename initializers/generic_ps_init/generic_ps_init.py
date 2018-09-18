14f7ad46419514af1ef33ef81d6d70e3fc887b35
Donald Batchelor <dbb@icx.net>
2018-09-17 23:44:54 -0400

    Implementing init_mode = mixed in generic_ps_init

--- initializers/generic_ps_init/generic_ps_init.py
+++ initializers/generic_ps_init/generic_ps_init.py
@@ -359,7 +359,7 @@ class generic_ps_init (Component):
             # mdescr/sconfig and construct cclist for generic_ps_init.f90
             if init_mode in ['mixed', 'MIXED'] :
                 mdescr_components =  self.get_component_param(services, 'MDESCR_COMPONENTS')
-                cclist = [0 for i in range(len(component.dict))]
+                cclist = [0 for i in range(len(component_dict))]
                 for comp in mdescr_components:
                     if comp.upper() not in component_dict.keys():
                         message = 'generic_ps_init: Unknown IPS component ' + comp
