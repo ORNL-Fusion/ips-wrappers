dbb7d1a8b50309e75a774b879321c6e786ce7126
Donald Batchelor <dbb@icx.net>
2018-09-17 23:53:21 -0400

    Implementing init_mode = mixed in generic_ps_init

--- initializers/generic_ps_init/generic_ps_init.py
+++ initializers/generic_ps_init/generic_ps_init.py
@@ -359,6 +359,7 @@ class generic_ps_init (Component):
             # mdescr/sconfig and construct cclist for generic_ps_init.f90
             if init_mode in ['mixed', 'MIXED'] :
                 mdescr_components =  self.get_component_param(services, 'MDESCR_COMPONENTS')
+                print 'mdescr_components =', mdescr_components
                 cclist = [0 for i in range(len(component_dict))]
                 for comp in mdescr_components:
                     print 'comp = ', comp
