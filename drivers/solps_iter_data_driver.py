da44da2b6506bc441c07dc6ecd5d951c1c13fca8
Tim Younkin <tyounkin19@gmail.com>
2020-03-18 23:28:17 -0400

    updates to solps_iter_process_data component by abstracting analysis to solps and gitr.py routines

--- drivers/solps_iter_data_driver.py
+++ drivers/solps_iter_data_driver.py
@@ -9,28 +9,36 @@ class solps_iter_data_driver(Component):
         print('Created %s' % (self.__class__))
 
     def init(self, timeStamp=0.0):
+        print('solps-iter-data-driver: beginning init call') 
 
-        solps_plasma_state_files = self.services.get_config_param('PLASMA_STATE_FILES')
+        solps_plasma_state_files = self.services.get_config_param('STATE_FILES')
+        
         ##split filenames into a list of strings
         file_list = solps_plasma_state_files.split()
-        ##loop over file names and create dummy files in ftridynInit work area
+        
+        ##loop over file names and create dummy files in init work area
         for index in range(len(file_list)):
             if os.path.isfile(file_list[index]) == 0 :
                 open(file_list[index], 'a').close()
 
         #update plasma state from relevant files in ftridynInit work area
-        self.services.update_plasma_state()        
+        self.services.update_state()        
+        
         return
 
     def step(self, timeStamp=0.0):
-        print('solss-iter-data-driver: beginning step call') 
+        print('solps-iter-data-driver: beginning step call') 
+        
         try:
             worker_comp = self.services.get_port('WORKER')
         except Exception:
             self.services.exception('Error accessing worker component')
             raise
+        
         self.services.call(worker_comp, 'step', 0.0)
+        
         print('solps-iter-data-driver: finished worker call') 
+        
         return
 
     def finalize(self, timeStamp=0.0):
