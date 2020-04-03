a6d7d61cc827c637f2f6f945bfb15a5fb2a88d51
Mark Cianciosa <cianciosamr@ornl.gov>
2020-04-03 09:50:57 -0400

    Add dictionaries to locate the output parameters and input parameters. WIP

--- ips-massive-serial-runner/massive_serial_runner.py
+++ ips-massive-serial-runner/massive_serial_runner.py
@@ -10,6 +10,99 @@ from component import Component
 from utilities import ScreenWriter
 from utilities import ZipState
 import os
+import json
+
+#-------------------------------------------------------------------------------
+#
+#  Dictionary of fastran outputs and their prefix codes.
+#
+#-------------------------------------------------------------------------------
+parameter = {
+    'q95'         : {'prefix' : 'aeqdisk',        'name' : 'q95',         'depends' : []                                                     },
+    'peak'        : {'prefix' : 'aeqdisk',        'name' : 'peak',        'depends' : []                                                     },
+    'li'          : {'prefix' : 'aeqdisk',        'name' : 'li',          'depends' : []                                                     },
+    'qmin'        : {'prefix' : 'aeqdisk',        'name' : 'qmin',        'depends' : []                                                     },
+    'betap'       : {'prefix' : 'aeqdisk',        'name' : 'betap',       'depends' : []                                                     },
+    'rho_qmin'    : {'prefix' : 'aeqdisk',        'name' : 'rho_qmin',    'depends' : []                                                     },
+    'betan_efit'  : {'prefix' : 'aeqdisk',        'name' : 'betan',       'depends' : []                                                     },
+    'ne'          : {'prefix' : 'fastran',        'name' : 'ne',          'depends' : []                                                     },
+    'ni'          : {'prefix' : 'fastran',        'name' : 'ni',          'depends' : []                                                     },
+    'te'          : {'prefix' : 'fastran',        'name' : 'te',          'depends' : []                                                     },
+    'ti'          : {'prefix' : 'fastran',        'name' : 'ti',          'depends' : []                                                     },
+    'q'           : {'prefix' : 'fastran',        'name' : 'q',           'depends' : []                                                     },
+    'rmajor'      : {'prefix' : 'fastran',        'name' : 'rmajor',      'depends' : []                                                     },
+    'aminor'      : {'prefix' : 'fastran',        'name' : 'aminor',      'depends' : []                                                     },
+    'zeff'        : {'prefix' : 'fastran',        'name' : 'zeff',        'depends' : []                                                     },
+    'kappa'       : {'prefix' : 'fastran',        'name' : 'delta',       'depends' : []                                                     },
+    'delta'       : {'prefix' : 'fastran',        'name' : 'kappa',       'depends' : []                                                     },
+    'ip'          : {'prefix' : 'fastran',        'name' : 'ip',          'depends' : []                                                     },
+    'bt'          : {'prefix' : 'fastran',        'name' : 'b0',          'depends' : []                                                     },
+    'r'           : {'prefix' : 'fastran',        'name' : 'r0',          'depends' : []                                                     },
+    'a'           : {'prefix' : 'fastran',        'name' : 'a0',          'depends' : []                                                     },
+    'we'          : {'prefix' : 'fastran',        'name' : 'we',          'depends' : []                                                     },
+    'wi'          : {'prefix' : 'fastran',        'name' : 'wi',          'depends' : []                                                     },
+    'wb'          : {'prefix' : 'fastran',        'name' : 'wb',          'depends' : []                                                     },
+    'betan'       : {'prefix' : 'fastran',        'name' : 'betan',       'depends' : []                                                     },
+    'nebar'       : {'prefix' : 'fastran',        'name' : 'nebar',       'depends' : []                                                     },
+    'tea'         : {'prefix' : 'fastran',        'name' : 'tea',         'depends' : []                                                     },
+    'tia'         : {'prefix' : 'fastran',        'name' : 'tia',         'depends' : []                                                     },
+    'taue'        : {'prefix' : 'fastran',        'name' : 'taue',        'depends' : []                                                     },
+    'taui'        : {'prefix' : 'fastran',        'name' : 'taui',        'depends' : []                                                     },
+    'tauth'       : {'prefix' : 'fastran',        'name' : 'tauth',       'depends' : []                                                     },
+    'tautot'      : {'prefix' : 'fastran',        'name' : 'tautot',      'depends' : []                                                     },
+    'tautot'      : {'prefix' : 'fastran',        'name' : 'tautot',      'depends' : []                                                     },
+    'tau89'       : {'prefix' : 'fastran',        'name' : 'tau89',       'depends' : []                                                     },
+    'tau98'       : {'prefix' : 'fastran',        'name' : 'tau98',       'depends' : []                                                     },
+    'pnbe'        : {'prefix' : 'fastran',        'name' : 'pnbe',        'depends' : []                                                     },
+    'pnbi'        : {'prefix' : 'fastran',        'name' : 'pnbi',        'depends' : []                                                     },
+    'prfe'        : {'prefix' : 'fastran',        'name' : 'prfe',        'depends' : []                                                     },
+    'prfi'        : {'prefix' : 'fastran',        'name' : 'prfi',        'depends' : []                                                     },
+    'prad'        : {'prefix' : 'fastran',        'name' : 'prad',        'depends' : []                                                     },
+    'pei'         : {'prefix' : 'fastran',        'name' : 'pei',         'depends' : []                                                     },
+    'poh'         : {'prefix' : 'fastran',        'name' : 'poh',         'depends' : []                                                     },
+    'ibs'         : {'prefix' : 'fastran',        'name' : 'ibs',         'depends' : []                                                     },
+    'inb'         : {'prefix' : 'fastran',        'name' : 'inb',         'depends' : []                                                     },
+    'irf'         : {'prefix' : 'fastran',        'name' : 'irf',         'depends' : []                                                     },
+    'pfuse_equiv' : {'prefix' : 'fastran',        'name' : 'pfuse_equiv', 'depends' : []                                                     },
+    'pfusi_equiv' : {'prefix' : 'fastran',        'name' : 'pfusi_equiv', 'depends' : []                                                     },
+    'rho'         : {'prefix' : 'fastran',        'name' : 'rho',         'depends' : []                                                     },
+    'rhob'        : {'prefix' : 'fastran',        'name' : 'rhob',        'depends' : []                                                     },
+    'volp'        : {'prefix' : 'fastran',        'name' : 'volp',        'depends' : []                                                     },
+    'pe_rf'       : {'prefix' : 'fastran',        'name' : 'pe_rf',       'depends' : []                                                     },
+    'pfus0'       : {'prefix' : 'outone',         'name' : 'pfus0',       'depends' : []                                                     },
+    'tauR'        : {'prefix' : 'get_tauR',       'name' : 'tauR',        'depends' : ['sigma', 'rho', 'rhob']                               },
+    'sigma'       : {'prefix' : 'sigma_hirshman', 'name' : 'sigma',       'depends' : ['ne', 'te', 'aminor', 'rmajor', 'zeff', 'q']          },
+    'neavg'       : {'prefix' : 'get_neavg',      'name' : 'sigma',       'depends' : ['volp', 'rhob', 'ne']                                 },
+    'rho_ec'      : {'prefix' : 'get_rho_ec',     'name' : 'rho_ec',      'depends' : ['rho', 'pe_rf']                                       },
+    'q0'          : {'prefix' : 'get_q0',         'name' : 'q0',          'depends' : ['q']                                                  },
+    'nus_core'    : {'prefix' : 'nues',           'name' : 'nus_core',    'depends' : ['rmajor', 'aminor', 'ne', 'te', 'zeff', 'q', 'i_core']},
+    'kappa1'      : {'prefix' : 'get_kappa1',     'name' : 'kappa1',      'depends' : ['kappa']                                              },
+    'delta1'      : {'prefix' : 'get_delta1',     'name' : 'delta1',      'depends' : ['delta']                                              },
+    'area'        : {'prefix' : 'get_area',       'name' : 'area',        'depends' : []                                                     },
+    'area1'       : {'prefix' : 'get_area1',      'name' : 'area1',       'depends' : ['r', 'a', 'kappa', 'delta']                           },
+    'pinj'        : {'prefix' : 'get_pinj',       'name' : 'pinj',        'depends' : ['prfe', 'prfi', 'pnbe', 'pnbi']                       },
+    'pinj_ei'     : {'prefix' : 'get_pinj_ei',    'name' : 'pink_ei',     'depends' : ['prfe', 'pnbe', 'pinj']                               },
+    'pfus'        : {'prefix' : 'get_pfus',       'name' : 'pfus',        'depends' : ['pfuse_equiv', 'pfusi_equiv']                         },
+    'qdt'         : {'prefix' : 'get_qdt',        'name' : 'qdt',         'depends' : ['pfus', 'pinj']                                       },
+    'wmhd'        : {'prefix' : 'get_wmhd',       'name' : 'wmhd',        'depends' : ['we', 'wi', 'wb']                                     },
+    'fni'         : {'prefix' : 'get_fni',        'name' : 'fni',         'depends' : ['inb', 'irf', 'ibs' 'ip']                             },
+    'fbs'         : {'prefix' : 'get_fbs',        'name' : 'fbs',         'depends' : ['ibs', 'ip']                                          },
+    'fnb'         : {'prefix' : 'get_fnb',        'name' : 'fnb',         'depends' : ['inb', 'ip']                                          },
+    'frf'         : {'prefix' : 'get_frf',        'name' : 'frf',         'depends' : ['irf', 'ip']                                          },
+    'h98'         : {'prefix' : 'get_h98',        'name' : 'h98',         'depends' : ['tauth', 'tau98']                                     },
+    'fgw'         : {'prefix' : 'get_fgw',        'name' : 'fgw',         'depends' : ['nebar', 'ip', 'a']                                   },
+    'fgw_ped'     : {'prefix' : 'get_fgw_ped',    'name' : 'fgw_ped',     'depends' : ['neped', 'ip', 'a']                                   },
+    'pmhd'        : {'prefix' : 'get_pmhd',       'name' : 'pmhd',        'depends' : ['b', 'ip', 'a', 'betan', 'mu0']                       },
+    'nus_edge'    : {'prefix' : 'constant',       'name' : 'nus_edge',    'depends' : []                                                     },
+    'i_core'      : {'prefix' : 'constant',       'name' : 'i_core',      'depends' : []                                                     },
+    'mu0'         : {'prefix' : 'constant',       'name' : 'mu0',         'depends' : []                                                     }
+}
+
+constants = {
+    'i_core'   : 50,
+    'nus_edge' : 0.0,
+    'mu0'      : math.pi*4.0E-7
+}
 
 #-------------------------------------------------------------------------------
 #
@@ -29,16 +122,18 @@ class massive_serial_runner(Component):
         ScreenWriter.screen_output(self, 'verbose', 'massive_serial_runner: init')
 
 #  Get config filenames.
-        if timeStamp == 0.0:
-            self.current_batch = self.services.get_config_param('CURRENT_MSR_BATCH')
+        if (timeStamp == 0.0):
             self.current_state = self.services.get_config_param('CURRENT_MSR_STATE')
+            self.current_batch = self.services.get_config_param('CURRENT_MSR_BATCH')
+            self.model_config = self.services.get_config_param('MODEL_CONFIG')
+            self.fastran_config = self.services.get_config_param('FASTRAN_CONFIG')
 
 #  Stage state.
         self.services.stage_state()
 
 #  Unzip files from the state. Use mode a so files an be read and written to.
         self.zip_ref = ZipState.ZipState(self.current_state, 'a')
-        self.zip_ref.extract(self.current_batch)
+        self.zip_ref.extract('inscan')
 
 #-------------------------------------------------------------------------------
 #
@@ -48,6 +143,29 @@ class massive_serial_runner(Component):
     def step(self, timeStamp=0.0):
         ScreenWriter.screen_output(self, 'verbose', 'massive_serial_runner: step')
 
+        flags = self.zip_ref.get_state()
+
+#  Run the massive serial workflow.
+        if 'state' in flags and flags['state'] == 'needs_update':
+            with open(current_batch, 'r') as json_ref:
+                keys = json.load(json_ref).keys()
+
+            task_wait = self.services.launch_task(MAX(self.NPROC, len(keys)),
+                                                  self.services.get_working_dir(),
+                                                  self.services.MASSIVE_SERIAL_EXE,
+                                                  'inscan',
+                                                  self.fastran_config,
+                                                  MAX(self.NPROC, len(keys)),
+                                                  1)
+
+#  Collect results of the workflow.
+            if (self.services.wait_task(task_wait)):
+                self.services.error('massive_serial_runner: step failed.')
+
+
+
+#  Append output data to batch. Need to know what data to read.
+
 #-------------------------------------------------------------------------------
 #
 #  Massive Serial Runner Component finalize method. This cleans up
@@ -56,3 +174,41 @@ class massive_serial_runner(Component):
 #-------------------------------------------------------------------------------
     def finalize(self, timeStamp=0.0):
         ScreenWriter.screen_output(self, 'verbose', 'massive_serial_runner: finalize')
+
+#-------------------------------------------------------------------------------
+#
+#  Load data parameter. Data parameters are listed in the parameters dictionary.
+#  for a given parameter, start by loading the dependencies. Then based on the
+#  prefix value load the data from the appropriate source. To avoid file io
+#  overhead, a file will be first checked it is opened. Opened files will be
+#  added to a list to cache the file reference. This updates the data base with
+#  the newly loaded parameter.
+#
+#  FIXME: Not sure how I should update for the mutilple directories so start with just one.
+#
+#-------------------------------------------------------------------------------
+    def load_parameter(self, directory, parameter, data_base):
+
+#  First check that the parameter is not already loaded.
+        if parameter in data_base:
+            return data_base
+
+#  Next load any dependencies.
+        for key in parameters[parameter][depends]:
+            data_base = self.load_parameter(directory, key, parameters)
+
+#  Now load the parameter.
+
+#-------------------------------------------------------------------------------
+#
+#  Load a parameter from a constant.
+#
+#-------------------------------------------------------------------------------
+    def constant(parameter):
+        return constants[parameter]
+
+#-------------------------------------------------------------------------------
+#
+#
+#
+#-------------------------------------------------------------------------------
--- ips-massive-serial-runner/massive_serial_runner_init.py
+++ ips-massive-serial-runner/massive_serial_runner_init.py
@@ -10,6 +10,56 @@ from component import Component
 from utilities import ScreenWriter
 from utilities import ZipState
 import os
+import json
+
+#-------------------------------------------------------------------------------
+#
+#  Dictionary of fastran inputs and their prefix codes.
+#
+#-------------------------------------------------------------------------------
+parameter = {
+    'r'           : {'prefix' : 'fastran_init', 'name' : 'r0'              },
+    'a'           : {'prefix' : 'fastran_init', 'name' : 'a0'              },
+    'kappa'       : {'prefix' : 'fastran_init', 'name' : 'kappa'           },
+    'delta'       : {'prefix' : 'fastran_init', 'name' : 'delta'           },
+    'bt'          : {'prefix' : 'fastran_init', 'name' : 'b0'              },
+    'ip'          : {'prefix' : 'fastran_init', 'name' : 'ip'              },
+    'zeffped'     : {'prefix' : 'fastran_init', 'name' : 'zeffped'         },
+    'm'           : {'prefix' : 'fastran_init', 'name' : 'm'               },
+    'xmid'        : {'prefix' : 'fastran_init', 'name' : 'xmid'            },
+    'xwid'        : {'prefix' : 'fastran_init', 'name' : 'xwid'            },
+    'ne0'         : {'prefix' : 'fastran_init', 'name' : 'ne0'             },
+    'neped'       : {'prefix' : 'fastran_init', 'name' : 'neped'           },
+    'nesep'       : {'prefix' : 'fastran_init', 'name' : 'nesep'           },
+    'alpha_ne'    : {'prefix' : 'fastran_init', 'name' : 'alpha_ne'        },
+    'beta_ne'     : {'prefix' : 'fastran_init', 'name' : 'beta_ne'         },
+    'te0'         : {'prefix' : 'fastran_init', 'name' : 'te0'             },
+    'teped'       : {'prefix' : 'fastran_init', 'name' : 'teped'           },
+    'tesep'       : {'prefix' : 'fastran_init', 'name' : 'tesep'           },
+    'alpha_te'    : {'prefix' : 'fastran_init', 'name' : 'alpha_te'        },
+    'beta_te'     : {'prefix' : 'fastran_init', 'name' : 'beta_te'         },
+    'ti0'         : {'prefix' : 'fastran_init', 'name' : 'ti0'             },
+    'tiped'       : {'prefix' : 'fastran_init', 'name' : 'tiped'           },
+    'tisep'       : {'prefix' : 'fastran_init', 'name' : 'tisep'           },
+    'alpha_ti'    : {'prefix' : 'fastran_init', 'name' : 'alpha_ti'        },
+    'beta_ti'     : {'prefix' : 'fastran_init', 'name' : 'beta_ti'         },
+    'omega0'      : {'prefix' : 'fastran_init', 'name' : 'omega0'          },
+    'alpha_omega' : {'prefix' : 'fastran_init', 'name' : 'alpha_omega'     },
+    'beta_omega'  : {'prefix' : 'fastran_init', 'name' : 'beta_omega'      },
+    'zeff0'       : {'prefix' : 'fastran_init', 'name' : 'zeff0'           },
+    'pe'          : {'prefix' : 'hcd_model'   , 'name' : 'pe'              },
+    'pi'          : {'prefix' : 'hcd_model'   , 'name' : 'pi'              },
+    'pinja_0'     : {'prefix' : 'nubeam'      , 'name' : 'pinja_0'         },
+    'pinja_1'     : {'prefix' : 'nubeam'      , 'name' : 'pinja_1'         },
+    'rfpow_0'     : {'prefix' : 'toray'       , 'name' : 'rfpow_0'         },
+    'thet_0'      : {'prefix' : 'toray'       , 'name' : 'rfpow_0'         },
+    'phai_0'      : {'prefix' : 'toray'       , 'name' : 'phai_0'          },
+    'thgrill_0'   : {'prefix' : 'genray'      , 'name' : 'grill_thgrill_0' },
+    'frqncy_0'    : {'prefix' : 'genray'      , 'name' : 'WAVE_FRQNCY_0'   },
+    'powers_0'    : {'prefix' : 'genray'      , 'name' : 'GRILL_POWERS_0'  },
+    'rho_jpeak'   : {'prefix' : 'modeleq_init', 'name' : 'RHO_JPEAK'       },
+    'jaxis'       : {'prefix' : 'modeleq_init', 'name' : 'JAXIS'           }
+}
 
 #-------------------------------------------------------------------------------
 #
@@ -32,6 +82,7 @@ class massive_serial_runner_init(Component):
 #  Get config filenames.
         current_batch = self.services.get_config_param('CURRENT_MSR_BATCH')
         current_state = self.services.get_config_param('CURRENT_MSR_STATE')
+        model_config = self.services.get_config_param('MODEL_CONFIG')
 
 #  Remove old inputs.
         for file in os.listdir('.'):
@@ -40,13 +91,22 @@ class massive_serial_runner_init(Component):
 #  Stage input files and setup inital state.
         self.services.stage_input_files(self.INPUT_FILES)
 
+        if os.path.exists(current_batch):
+            if not os.path.exists(current_batch):
+                raise Exception('Model config {} not found.'.format(model_config))
+
+            with open(current_batch, 'r') as json_ref:
+                create_input(json.load(json_ref))
+
 #  Create plasma state from files. Input files can either be a new plasma state,
 #  training data file or both. If both file were staged, replace the training
 #  data input file. If the training data file is present flag the plasma state
 #  as needing to be updated.
         with ZipState.ZipState(current_state, 'a') as zip_ref:
-            if os.path.exists(current_batch):
+            if os.path.exists('inscan'):
+                zip_ref.write('inscan')
                 zip_ref.write(current_batch)
+                zip_ref.write(model_config)
                 zip_ref.set_state(state='needs_update')
 
         self.services.update_state()
@@ -67,3 +127,45 @@ class massive_serial_runner_init(Component):
 #-------------------------------------------------------------------------------
     def finalize(self, timeStamp=0.0):
         ScreenWriter.screen_output(self, 'verbose', 'massive_serial_runner_init: finalize')
+
+#-------------------------------------------------------------------------------
+#
+#
+#
+#-------------------------------------------------------------------------------
+    def create_input(data_base):
+        keys = data_base.keys()
+
+        input = '{}'.format(set_header('TIME_ID'))
+
+        for key in keys:
+            input = '{} {}'.format(input, set_header(key))
+
+        input = '{}\n'.format(input)
+
+        for i in range(len(data_base[keys[0]])):
+            input = '{}{:05d}'.format(input, i)
+
+            for key in keys:
+                input = '{} {}'.format(input, data_base[key][i])
+
+            input = '{}\n'.format(input)
+
+        with open('inscan', 'w') as input_ref:
+            input_ref.write(input)
+
+#-------------------------------------------------------------------------------
+#
+#
+#
+#-------------------------------------------------------------------------------
+    def set_header(key):
+        return '{}:{}:{}'.format(get_prefix(key), key.toupper(), 'float')
+
+#-------------------------------------------------------------------------------
+#
+#
+#
+#-------------------------------------------------------------------------------
+    def get_prefix(key):
+        return parameter[key.tolower()]['prefix']
