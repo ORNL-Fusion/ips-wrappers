15b8ff3b743c58d8ea72a01b572a1301b239b320
Donald Batchelor <dbb@icx.net>
2020-04-21 15:28:07 -0400

    Update get_IPS_config_parameters.py simple_file_editing_functions.py to P3

--- utilities/simple_file_editing_functions.py
+++ utilities/simple_file_editing_functions.py
@@ -1,20 +1,15 @@
 #! /usr/bin/env python
 
 """
-simple_file_editing.py 9/3/2018 (Batchelor)
-A collection of simple routines for reading, editing and writing text files.
+simple_assignment_file_edit.py 3/13/2018 (Batchelor)
 Utilities to read, modify, and write text files with lines in the form of assignment
-statements, i.e. of the form <name> = <value>. For now it only deals with single line 
-assignments.
-
-There is also a more specialized function for modifying variables in a fortran namelist
-file which can handle variables on multiple lines.
+statements, i.e. of the form <name> = <value>
+For now it only deals with single line assignments.
 """
 
 # Working notes:
-# 9/3/2018 DBB
-# This was produced by merging the previous simple_file_editing.py functions with some
-# rudimentary fortran namelist editing functions.
+# Batchelor 4/21/2020: Copied futurized component from dbb4 branch
+#
 
 import sys
 
@@ -50,7 +45,7 @@ def lines_to_variable_dict(lines):
 
 def variable_dict_to_lines(variable_dict):
     lines = []
-    for var in variable_dict.keys():
+    for var in list(variable_dict.keys()):
         lines.append(var + ' = ' + str(variable_dict[var]) + '\n')
     return lines
 
@@ -67,7 +62,7 @@ def variable_dict_to_output_file(variable_dict, filename):
 
 def add_variables_to_output_file(variable_dict, filename):
     lines = get_lines(filename)
-    for name in variable_dict.keys():
+    for name in list(variable_dict.keys()):
         new_line = name + ' = ' + str(variable_dict[name]) + '\n'
         lines.append(new_line)
     put_lines(filename, lines)
@@ -89,110 +84,33 @@ def modify_variables_in_file(change_dict, filename):
         if '=' in line:
             name, val = line.split("=")
             name = name.strip()
-            if name in change_dict.keys():
+            if name in list(change_dict.keys()):
                 lines[i] = name + ' = ' + str(change_dict.pop(name)) + '\n'
                 
     if len(change_dict) != 0:
-        for name in change_dict.keys():
-            print 'variable to be modified ', name, ' not found in file ', filename
+        for name in list(change_dict.keys()):
+            print('variable to be modified ', name, ' not found in file ', filename)
             raise
     put_lines(filename, lines)
 
-#---------------------------------------------------------------------------------------
-# Edit fortran namelist file
-#---------------------------------------------------------------------------------------
+#_________________________________________________________________________________________________
 
-# This function allows changing the variable values in a fortran namelist file. 
-# Arguments:
-#
-# lines = List of character strings representing the of lines from the input namelist file.
-#    For example obtained from the get_lines() function above
-#
-# var = String containing the name of the variable to be changed.
-#
-# values = Data to go into var.  A scalar or list of things python can convert to a string
-#     The editing that can be done is pretty rudimentary.  
-#     If var is a vector then value must contain the whole new vector.  
-#     For example it doesn't support changing an array element or array slice.
-#     
-# separator = Optional argument specifying the separator between array elements.  The
-#     the default is comma
-#
-# Returns the lines with the new data
-#
-# N.B. This code writes big float numbers in Python form i.e. x.yyyyEzz.  If the fortran
-# variable is declared double, the compiler will most likely upconvert the (e) form.  At
-# least INTEL does.
+if __name__ == '__main__':
 
-def edit_nml_file(lines, var, values, separator = ','):
+    VD = {'x': 1.0, 'y': 2.000001}
+    lines = variable_dict_to_lines(VD)
+    print('lines = ', lines)
 
-    # Find the line in the namelist containing 'var = ' and get rid of newline if present.
-    var_line_number = -1
-    for i in range(len(lines)):
-        line = lines[i]
-        if '=' in line:
-            split_line = line.split('=')
-            #print 'split_line = ', split_line
-            if (split_line[0].strip()).lower() == var.lower():
-                var_line_number = i
-
-    if var_line_number == -1:
-        message = 'Could not find variable ', var, ' in namelist lines'
-        print message
-        raise Exception(message)
-        
-    print 'var_line_number = ', var_line_number
-    print 'lines[var_line_number] = ', lines[var_line_number]
+    variable_dict = lines_to_variable_dict(lines)
+    print('variable_dict = ', variable_dict)
     
-    if lines[var_line_number][-1] == '\n':
-        lines[var_line_number] = lines[var_line_number][:-1]
+    variable_dict_to_output_file(VD, 'out_file')
 
-    # Try to find out how many lines of text go with this variable.  So find the next
-    # line with either an '=' sign or the end of the namelist group indicated by a '/' 
-    # at the end of a line or a '&' at the beginning of a line. 
-    var_lines = 1
-    test = False
-    while test == False:
-        next_line_no = var_line_number + var_lines
-        if next_line_no >= len(lines):   # End of file
-            test = True
-            continue        
-        next_line = lines[next_line_no]
-        if '=' in next_line:   # Could get fooled by = in a quoted string
-            test = True
-            eq_index = next_line.find('=') # so check if quote before =
-            single_quote_index = next_line.find("'")
-            if single_quote_index > -1 and single_quote_index < eq_index:
-                test = False
-            double_quote_index = next_line.find('"')
-            if double_quote_index > -1 and double_quote_index < eq_index:
-                test = False
-        elif next_line[-1] == '/':  # At end of line means end of group
-            test = True
-        elif next_line[0] == '&':  # At front of line means end of group
-            test = True
-        else:
-            var_lines += 1
-            
-    # Insert line with new values at lines[i]
-    lines[var_line_number] = lines[var_line_number].split('=')[0] + ' = '
-    if type(values) in [type([]), type(())]:
-        for val in values:
-            if type(val) == type('str'):
-                val_str = "\'" + val + "\'"
-            else:
-                val_str = str(val)
-            lines[var_line_number] = lines[var_line_number] + val_str + ', '
-    else:   # Values not a list, val = values
-    	print var, ' = ', values, ' type = ', type(values)
-        val = values
-        if type(val) == type('str'):
-            val_str = "\'" + val + "\'"
-            print 'val_str = ', val_str
-        else:
-            val_str = str(val)
-        lines[var_line_number] = lines[var_line_number] + str(val_str)
-    lines[var_line_number] = lines[var_line_number] + '\n'
-    print 'New ', lines[var_line_number]
-        
-    return lines[:var_line_number + 1] + lines[var_line_number + var_lines:]
+    VD_2 = input_file_to_variable_dict('little_dict2')
+    print(VD_2)
+
+    change_dict = {'x': [113.0, 142], 'y': 22.000001, 'switch' : "'on'"}
+    modify_variables_in_file(change_dict, 'little_dict2')
+
+    change_dict = {'Q': 600}
+    add_variables_to_output_file(change_dict, 'test_file_2')
