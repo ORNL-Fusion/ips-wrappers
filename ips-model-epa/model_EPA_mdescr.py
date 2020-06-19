96f7631fd0d389035bb0da452510d6fff31b3f59
Donald Batchelor <dbb@icx.net>
2020-06-18 22:43:40 -0400

    Update model_EPA_mdescr.py from dbb4

--- ips-model-epa/model_EPA_mdescr.py
+++ ips-model-epa/model_EPA_mdescr.py
@@ -50,7 +50,13 @@ The list of evolution models implemented so far is very short, two:
 """
 
 # ------------------------------------------------------------------------------
-# Working Notes
+# Working Notes:
+
+# 9-12-2019
+# Eliminated import get_lines, put_lines, edit_nml_file, get_global_param, and
+# get_component_param.  Get these from /ips-wrappers/utilities.  Needs to be on 
+# PYTHON_PATH.
+
 # 3-31-2017
 # Added INIT_ONLY
 # ------------------------------------------------------------------------------
@@ -65,6 +71,8 @@ import string
 import math
 from netCDF4 import *
 from  component import Component
+from simple_file_editing_functions import get_lines, put_lines, edit_nml_file
+from get_IPS_config_parameters import get_global_param, get_component_param
 
 parameterList = ['Te_0', 'Te_edge', 'alpha_Te_1', 'alpha_Te_2', 'ne_0', 'ne_edge',\
     'alpha_ne_1', 'alpha_ne_2',\
@@ -96,7 +104,7 @@ class model_EPA_mdescr(Component):
         self.services.stage_plasma_state()
 
         self.services.stage_input_files(self.INPUT_FILES)
-        cur_state_file = self.services.get_config_param('CURRENT_STATE')
+        cur_state_file = get_global_param(self, services,'CURRENT_STATE')
         bin = os.path.join(self.BIN_PATH, 'model_EPA_mdescr')
 
         print 'Executing ', [bin, cur_state_file, 'INIT', timeStamp]
@@ -134,7 +142,7 @@ class model_EPA_mdescr(Component):
 
     def step(self, timeStamp):
         services = self.services
-        init_only = self.try_get_component_param(services, 'INIT_ONLY', optional = True)
+        init_only = get_component_param(self, services, 'INIT_ONLY', optional = True)
         if init_only in ['TRUE', 'True', 'true']: return
 
         print 'model_EPA_mdescr.step() called'
@@ -143,14 +151,14 @@ class model_EPA_mdescr(Component):
 
 # Copy current and prior state over to working directory
         services.stage_plasma_state()
-        cur_state_file = services.get_config_param('CURRENT_STATE')
+        cur_state_file = get_global_param(self, services,'CURRENT_STATE')
         ps = Dataset(cur_state_file, 'r+', format = 'NETCDF3_CLASSIC')
         tinit = ps.variables['tinit'].getValue()
 
 # Time evolution of parameters
         # get lines from namelist file
-        inputLines = self.get_lines('model_EPA_mdescr_input.nml')
-        initial_nml_Lines = self.get_lines('initial_input.nml')
+        inputLines = get_lines('model_EPA_mdescr_input.nml')
+        initial_nml_Lines = get_lines('initial_input.nml')
 
         evolution_models = {'linear_DT': self.linear_DT,\
                             'ramp_initial_to_final': self.ramp_initial_to_final}
@@ -161,7 +169,7 @@ class model_EPA_mdescr(Component):
         # arguments
         params_to_change = False
         for param in parameterList:
-            model_name = self.try_get_component_param(services, param + '_DT_model', \
+            model_name = get_component_param(self, services, param + '_DT_model', \
                 optional = True)
             if model_name != None:
                 model_name = model_name.strip()
@@ -169,7 +177,7 @@ class model_EPA_mdescr(Component):
         
                 if model_name == 'ramp_initial_to_final':
                     print 'model_EPA_mdescr: ramp_initial_to_final'
-                    DT_paramsList = self.try_get_component_param(services, param + '_DT_params').split()
+                    DT_paramsList = get_component_param(self, services, param + '_DT_params').split()
                     t_initial = float(DT_paramsList[0])
                     t_final = float(DT_paramsList[1])
                     
@@ -186,7 +194,7 @@ class model_EPA_mdescr(Component):
         
                 if model_name == 'exp_initial_to_final':
                     print 'model_EPA_mdescr: exp_initial_to_final'
-                    DT_paramsList = self.try_get_component_param(services, param + '_DT_params').split()
+                    DT_paramsList = get_component_param(self, services, param + '_DT_params').split()
                     t_initial = float(DT_paramsList[0])
                     tau = float(DT_paramsList[1])
                     Value_final = float(DT_paramsList[2])
@@ -265,143 +273,3 @@ class model_EPA_mdescr(Component):
     def linear_DT(self, f0, t, t0, DT):
         return f0 + (t - t0)*DT
 
-    # Try to get config parameter - wraps the exception handling for get_config_parameter()
-    def try_get_config_param(self, services, param_name, optional=False):
-
-        try:
-            value = services.get_config_param(param_name)
-            print param_name, ' = ', value
-        except Exception:
-            if optional:
-                print 'config parameter ', param_name, ' not found'
-                value = None
-            else:
-                message = 'required config parameter ', param_name, ' not found'
-                print message
-                services.exception(message)
-                raise
-
-        return value
-
-    # Try to get component specific config parameter - wraps the exception handling
-    def try_get_component_param(self, services, param_name, optional=False):
-
-        if hasattr(self, param_name):
-            value = getattr(self, param_name)
-            print param_name, ' = ', value
-        elif optional:
-            print 'optional config parameter ', param_name, ' not found'
-            value = None
-        else:
-            message = 'required component config parameter ', param_name, ' not found'
-            print message
-            services.exception(message)
-            raise
-
-        return value
-
-
-    # A utility to do simple editing of txt files on a line by line basis.
-    #---------------------------------------------------------------------------------------
-    # Open an input file and return the lines
-    def get_lines(self, filename):
-        file = open(filename, 'r')
-        lines = file.readlines()
-        file.close()
-        return lines
-
-    #---------------------------------------------------------------------------------------
-    # Open an output file and write lines into it
-    def put_lines(self, filename, lines):
-        file = open(filename, 'w')
-        file.writelines(lines)
-        file.close()
-
-
-    #---------------------------------------------------------------------------------------
-    # Editing utilities
-    #---------------------------------------------------------------------------------------
-
-    def edit_nml_file(self, lines, var, values, separator = ','):
-
-        # Find the line in the namelist containing 'var = ' and get rid of newline if present.
-        var_line_number = -1
-        for i in range(len(lines)):
-            line = lines[i]
-            if '=' in line:
-                split_line = line.split('=')
-                #print 'split_line = ', split_line
-                if (split_line[0].strip()).lower() == var.lower():
-                    var_line_number = i
-
-        if var_line_number == -1:
-            message = 'Could not find variable ', var, ' in namelist lines'
-            print message
-            raise Exception(message)
-        
-        #print 'var_line_number = ', var_line_number
-        #print 'lines[var_line_number] = ', lines[var_line_number]
-    
-        if lines[var_line_number][-1] == '\n':
-            lines[var_line_number] = lines[var_line_number][:-1]
-
-        # Try to find out how many lines of text go with this variable.  So find the next
-        # line with either an '=' sign or a '/' signifying the end of the namelist group.
-        var_lines = 1
-        test = False
-        while test == False:
-            next_iine_no = var_line_number + var_lines
-            next_line = lines[next_iine_no]
-            if '=' in next_line:   # Could get fooled by = in a quoted string
-                test = True
-                eq_index = next_line.find('=') # so check if quote before =
-                single_quote_index = next_line.find("'")
-                if single_quote_index > -1 and single_quote_index < eq_index:
-                    test = False
-                double_quote_index = next_line.find('"')
-                if double_quote_index > -1 and double_quote_index < eq_index:
-                    test = False
-            elif  '/' in next_line[-2:] :  # At end of line means end of group
-                test = True
-            else:
-                var_lines += 1
-        print 'var_lines = ', var_lines     
-            
-        # Insert line with new values at lines[i]
-        lines[var_line_number] = lines[var_line_number].split('=')[0] + ' = '
-        for val in values:
-            lines[var_line_number] = lines[var_line_number] + str(val) + ', '
-        lines[var_line_number] = lines[var_line_number] + '\n'
-        print 'New ', lines[var_line_number]
-        
-        return lines[:var_line_number + 1] + lines[var_line_number + var_lines:]
-
-    def read_var_from_nml_lines(self, lines, var, separator = ','):
-    # This routine is very limited, for now it only reads scalar real numbers
-
-        # Find the line in the namelist containing 'var = ' 
-        var_line_number = -1
-        for i in range(len(lines)):
-            line = lines[i]
-            if '=' in line:
-                split_line = line.split('=')
-                #print 'split_line = ', split_line
-                if (split_line[0].strip()).lower() == var.lower():
-                    var_line_number = i
-
-        if var_line_number == -1:
-            message = 'read_var_from_nml_lines: Could not find variable ', var, ' in namelist lines'
-            print message
-            raise Exception(message)
-    
-        RHS = lines[var_line_number].split('=')[1]
-        
-    #     Get rid of newline if there is one
-        if RHS[-1] == '\n':
-            lines[var_line_number] = lines[var_line_number][:-1]
-    
-        RHS_list = RHS.split(',')
-    #    print 'RHS = ', RHS_list[0].split()
-        value = float(RHS_list[0].split()[0])
-        print 'value = ', value
-        return value
