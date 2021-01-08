cc0773637696830bef365f6345997f61f7d65a31
cianciosa <cianciosamr@ornl.gov>
2021-01-08 15:05:46 -0500

    Use python module instead of manual path to script.

---
+++ ips-vmec/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="ips_vmec",
+    version="1.0.0",
+    install_requires=["ips_component_utilities"],
+    packages=find_packages(),
+)
