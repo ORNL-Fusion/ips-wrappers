a41dca8fb6df8055fede30af8d0d12d054074ab9
Mark Cianciosa <cianciosamr@ornl.gov>
2021-04-30 16:11:50 -0400

    Update ml_train workflow to use new ips framework and install as a module.

---
+++ ips-ml-train/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="ml_train",
+    version="1.0.0",
+    install_requires=["ips_component_utilities"],
+    packages=find_packages(),
+)
