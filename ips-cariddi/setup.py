5685a89e52c392b6e9a4f78fbb791770d9297a4c
m4c <cianciosamr@ornl.gov>
2021-06-22 12:40:10 -0400

    Automatically install install dependecy workflows.

--- ips-cariddi/setup.py
+++ ips-cariddi/setup.py
@@ -1,4 +1,17 @@
 from setuptools import setup, find_packages
+from distutils.command.build import build
+from distutils.command.install import install
+import os
+
+class build_deps(build):
+    def run(self):
+        os.system("python3 ../ips-v3fit/setup.py build")
+        build.run(self)
+
+class install_deps(install):
+    def run(self):
+        os.system("sudo python3 ../ips-v3fit/setup.py install")
+        install.run(self)
 
 setup(
     name="ips_cariddi",
@@ -6,4 +19,8 @@ setup(
     install_requires=["ips_v3fit"],
     packages=find_packages(),
     scripts=["cariddi_pre.py","cariddi_bin.py"]
+    cmdclass={
+        'build'   : build_deps,
+        'install' : install_deps
+    },
 )
--- ips-siesta/setup.py
+++ ips-siesta/setup.py
@@ -1,8 +1,25 @@
 from setuptools import setup, find_packages
+from distutils.command.build import build
+from distutils.command.install import install
+import os
+
+class build_deps(build):
+    def run(self):
+        os.system("python3 ../ips-vmec/setup.py build")
+        build.run(self)
+
+class install_deps(install):
+    def run(self):
+        os.system("sudo python3 ../ips-vmec/setup.py install")
+        install.run(self)
 
 setup(
     name="ips_siesta",
     version="1.0.0",
     install_requires=["ips_vmec"],
     packages=find_packages(),
+    cmdclass={
+        'build'   : build_deps,
+        'install' : install_deps
+    },
 )
