320aa496ecbbbeab3b9209e517b41256ca56f7b2
cianciosa <cianciosamr@ornl.gov>
2021-06-22 15:01:50 -0400

    Use standalone omfit classes.

--- ips-v3fit/ips_v3fit/v3fit.py
+++ ips-v3fit/ips_v3fit/v3fit.py
@@ -8,8 +8,8 @@
 #-------------------------------------------------------------------------------
 
 from ipsframework import Component
-from omfit.classes.omfit_namelist import OMFITnamelist
-from omfit.classes.omfit_nc import OMFITnc
+from omfit_classes.omfit_namelist import OMFITnamelist
+from omfit_classes.omfit_nc import OMFITnc
 from ips_component_utilities import ZipState
 from ips_component_utilities import ScreenWriter
 from ips_component_utilities import NamelistItem
