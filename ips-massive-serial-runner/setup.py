485c0e8aacc278a9289c99397de844eedbc9f1e6
cianciosa <cianciosamr@ornl.gov>
2021-06-22 12:48:33 -0400

    Add dependency build and install for the ips-ml_train workflow.

--- ips-massive-serial-runner/setup.py
+++ ips-massive-serial-runner/setup.py
@@ -1,8 +1,25 @@
 from setuptools import setup, find_packages
+from distutils.command.build import build
+from distutils.command.install import install
+import os
+
+class build_deps(build):
+    def run(self):
+        os.system("python3 ../utilities/setup.py build")
+        build.run(self)
+
+class install_deps(install):
+    def run(self):
+        os.system("sudo python3 ../utilities/setup.py install")
+        install.run(self)
 
 setup(
     name="massive_serial_runner",
     version="1.0.0",
     install_requires=["ips_component_utilities"],
     packages=find_packages(),
+    cmdclass={
+        'build'   : build_deps,
+        'install' : install_deps
+    },
 )
--- ips-ml-train/setup.py
+++ ips-ml-train/setup.py
@@ -1,8 +1,24 @@
 from setuptools import setup, find_packages
+from distutils.command.build import build
+from distutils.command.install import install
+import os
+
+class build_deps(build):
+    def run(self):
+        os.system("python3 ../ips-massive-serial-runner/setup.py build")
+        build.run(self)
+
+class install_deps(install):
+    def run(self):
+        os.system("sudo python3 ../ips-massive-serial-runner/setup.py install")
+        install.run(self)
 
 setup(
     name="ml_train",
     version="1.0.0",
     install_requires=["massive_serial_runner"],
     packages=find_packages(),
-)
+    cmdclass={
+        'build'   : build_deps,
+        'install' : install_deps
+    },)
