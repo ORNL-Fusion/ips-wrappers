c37e11e4f5d70d5d05770f53ad697ac106c3f75f
cianciosa <cianciosamr@ornl.gov>
2022-08-12 14:00:46 -0700

    Add mpi4py to dependency list.

--- ips-iterative-xolotlFT/setup.py
+++ ips-iterative-xolotlFT/setup.py
@@ -4,7 +4,7 @@ setup(
     name="ips_xolotlFT",
     packages=["ips_xolotlFT","ips_xolotlFT.python_scripts_for_coupling"],
     version="1.0.0",
-    install_requires=["ipsframework","numpy","h5py","matplotlib"],
+    install_requires=["ipsframework","numpy","h5py","matplotlib","mpi4py"],
     scripts=["ips_xolotlFT/python_scripts_for_coupling/translate_ftridyn_to_xolotl.py",
              "ips_xolotlFT/python_scripts_for_coupling/get_yields.py"],
     package_data={"ips_xolotlFT.python_scripts_for_coupling": ["table1.txt"]},
