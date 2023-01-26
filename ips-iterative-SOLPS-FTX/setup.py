3fbd1a157874ab9bccd87afe0ed8565e1365c64f
AneLasa <lasaesquisaa@ornl.gov>
2023-01-26 14:39:07 -0800

    launch scripts instead of running them

--- ips-iterative-SOLPS-FTX/setup.py
+++ ips-iterative-SOLPS-FTX/setup.py
@@ -5,6 +5,6 @@ setup(
     packages=["ips_solps_ftx","ips_solps_ftx.python_scripts_for_coupling"],
     version="1.0.0",
     install_requires=["ipsframework","ips_xolotlFT","solps-iter"],
-    scripts=["ips_solps_ftx/python_scripts_for_coupling/binTRIDYN.py"],
+    scripts=["ips_solps_ftx/python_scripts_for_coupling/binTRIDYN.py","ips_solps_ftx/python_scripts_for_coupling/plasmaOut2ftxIn.py"],
     find_packages=find_packages()
 )
