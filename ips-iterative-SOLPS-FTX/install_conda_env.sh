97d2327aa817d380941a3aa572fd82c08f098785
AneLasa <lasaesquisaa@ornl.gov>
2023-05-01 19:37:05 -0700

    small environment changes from moving from Perlmutter to Cori and back

--- ips-iterative-SOLPS-FTX/install_conda_env.sh
+++ ips-iterative-SOLPS-FTX/install_conda_env.sh
@@ -1,3 +1,3 @@
 module swap PrgEnv-${PE_ENV,,} PrgEnv-gnu
 export MPICC="cc -shared"
-conda env create -f SOLPS-FTX_env.yml --force
+conda env create -f SOLPS-FTX_env.yml --force -p /global/homes/a/a7l/.conda/envs/solps-ftx
