56003b5682b109302df8df5aa28692600dc4793f
sfrnk <sfrnk@mit.edu>
2023-05-24 18:55:37 -0700

    overhauled toric input preparation

--- toric_cql3d_iterate/prepare_abj_input.f90
+++ toric_cql3d_iterate/prepare_abj_input.f90
@@ -6,39 +6,42 @@
            & swim_error
 
       implicit none
-
+      
       !Parameters
       INTEGER, PARAMETER :: PAR = 1, PERP = 2, LOWER = 1, UPPER = 2
       integer, parameter :: swim_string_length = 256
-      
+      INTEGER, PARAMETER:: FNLEN = 50, SPEC_NMLEN = 25
+      INTEGER, PARAMETER:: NSPEC_MAX = 4
+
       !program vars
       character(len =swim_string_length) :: cur_state_file, cur_geq_file
-      character *(25):: name_minspec,arg_enorm
+      character *(25):: name_minspec,arg_enorm,arg_nsurf,arg_specs
       double precision:: z_minspec, m_minspec,enorm
-      INTEGER :: ierr,isp_min,iarg,inp_unit, out_unit
-      logical :: lex, enorm_set
+      INTEGER :: ierr,isp_min,iarg,inp_unit, out_unit=108
+      logical :: lex, enorm_set, surf_set
       
       !abj driver namelist vars
-      CHARACTER *(20):: profile_datafile, distribution, profile
-      INTEGER :: numerical_species
+      CHARACTER *(20):: &
+      distribution, profile, profile_datafile
+  
       
       !abj namelist vars
-      CHARACTER *(50):: cdf_iofn
-      CHARACTER *(25):: species_name
-      INTEGER, DIMENSION(PAR:PERP) :: npts
-      INTEGER :: ntheta,debug_level
-      DOUBLE PRECISION, DIMENSION(PAR:PERP, LOWER:UPPER):: vlims
+      CHARACTER *(FNLEN):: cdf_iofn
+      CHARACTER *(SPEC_NMLEN),DIMENSION(NSPEC_MAX):: species_name
+      INTEGER, DIMENSION(PAR:PERP):: npts
+      INTEGER:: ntheta, debug_level
+      INTEGER:: ngen, npsi
       DOUBLE PRECISION, DIMENSION(LOWER:UPPER):: v_asymptotic
-      DOUBLE PRECISION:: Bmax_by_Bmin, velocity_normalization,Z_charge, &
-                         A_mass
-      
+      DOUBLE PRECISION:: Bmax_by_Bmin, velocity_normalization
+      DOUBLE PRECISION, DIMENSION(PAR:PERP, LOWER:UPPER):: vlims
+      DOUBLE PRECISION, DIMENSION(NSPEC_MAX):: Z_charge, A_mass 
+
       !namelists
-      NAMELIST / ABJ_driver_nml / profile_datafile, distribution, profile, &
-                                  numerical_species
+      NAMELIST / ABJ_driver_nml / profile_datafile, distribution, profile 
 
       NAMELIST / ABJ_nml / cdf_iofn, ntheta, npts, vlims, v_asymptotic, &
-                           Bmax_by_Bmin, Z_charge, A_mass, species_name, &
-                           velocity_normalization, debug_level
+              Bmax_by_Bmin, NPSI, NGEN, Z_Charge, A_mass, species_name, &
+              velocity_normalization, debug_level
 
       !read input args
       call get_arg_count(iarg)
@@ -54,11 +57,16 @@
          call get_arg(1,cur_state_file)
          call get_arg(2,arg_enorm)
          read(arg_enorm,*) enorm
+      CASE(3)
+         enorm_set = .True.
+         call get_arg(1,cur_state_file)
+         call get_arg(2,arg_enorm)
+         read(arg_enorm,*) enorm
+         call get_arg(3,arg_specs)
       end select
-
-
-      WRITE(*,*) 'cur_state enorm', cur_state_file, enorm
       
+      WRITE(*,*) 'cur_state enorm', cur_state_file, enorm
+  
       !get plasma state data
       call ps_get_plasma_state(ierr,trim(cur_state_file))
       if(ierr .ne. 0) stop 'cannot get plasma state'
@@ -89,48 +97,58 @@
       
       !set namelist values
 
-      !use plasma state values to set certain namelist params.
+      !use plasma state values to set certain namelist params
       isp_min = ps%rfmin_to_alla(1)
       z_minspec = 1.d0*NINT(ps%q_alla(isp_min)/ps_xe)
       m_minspec = 1.d0*NINT(ps%m_alla(isp_min)/ps_mp)
       name_minspec = trim(ps%alla_name(isp_min))
       
       !abj_driver_nml
-      profile_datafile = 'profnt.cdf'
+      profile_datafile = 'cql3d.nc'
       distribution = 'cql3d'
       profile = 'datafile'
 
       !abj_nml
       if(enorm_set)then
-         velocity_normalization = 2*3.106208817D7*(enorm/m_minspec) !this goes up to the normalized energy
-                                                                    !and assumes that E in keV m in amu v in cm/s 
-         vlims(PAR,LOWER) = -1.0
-         vlims(PAR,UPPER) = 1.0
+         velocity_normalization = 3.106208817D7*sqrt(0.5*enorm/m_minspec)/25.0
+         vlims(PAR,LOWER) = -25.0
+         vlims(PAR,UPPER) = 25.0
          vlims(PERP,LOWER) = 0.0
-         vlims(PERP,UPPER) = 1.0
-         v_asymptotic(LOWER) = -1.0
-         v_asymptotic(UPPER) = 1.0
+         vlims(PERP,UPPER) = 25.0
+         v_asymptotic(LOWER) = -25.0
+         v_asymptotic(UPPER) = 25.0
       endif
-      
-      Bmax_by_Bmin = 2.5
-      A_mass = m_minspec
-      Z_charge = z_minspec
-      species_name = name_minspec
+
       debug_level = 0
+      Bmax_by_Bmin = 2.0 !SF this is as high as this should ever need to be for an aspect ratio 3 or larger
 
-      !use the npts values found in the template file for now
+      if (trim(arg_specs).eq.'MIN')then
+         ngen = 1
+         A_mass(1) = m_minspec
+         Z_charge(1) = z_minspec
+         species_name(1) = name_minspec
+      endif
 
+      if (trim(arg_specs).eq.'MIN+')then
+         ngen = 2
+         A_mass(1) = m_minspec
+         Z_charge(1) = z_minspec
+         species_name(1) = name_minspec
+         A_mass(2) = 1.d0*NINT(ps%m_alla(1)/ps_mp)
+         Z_charge(2) = 1.d0*NINT(ps%q_alla(1)/ps_xe)
+         species_name(2) = trim(ps%alla_name(1))
+      endif
+         
       !write namelists to ABJ input files
-
       open(unit=out_unit, file = 'ABJ_driver.inp_new', delim = 'apostrophe', &
-     &     status = 'unknown', form = 'formatted') 
+          status = 'unknown', form = 'formatted') 
 
       write(out_unit, nml=ABJ_driver_nml)
       close(out_unit)
 
       open(unit=out_unit, file = 'ABJ.inp_new', delim = 'apostrophe', &
-     &     status = 'unknown', form = 'formatted') 
+          status = 'unknown', form = 'formatted') 
       write(out_unit, nml=ABJ_nml)
       close(out_unit)
-
+      
       end program prepare_abj_input
