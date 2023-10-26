b0ec55bbe613de594bb8501021c49e14b1182b5a
cianciosa <cianciosamr@ornl.gov>
2023-10-26 07:29:23 -0700

    Revert comand line flags.

--- ips-massive-parallel-runner/massive_parallel_runner/massive_parallel_runner_init.py
+++ ips-massive-parallel-runner/massive_parallel_runner/massive_parallel_runner_init.py
@@ -87,8 +87,8 @@ class massive_parallel_runner_init(Component):
                                                       self.SAMPLE_EXE,
                                                       '--input={}'.format(self.inscan_config_file),
                                                       '--output=inscan',
-                                                      '--nscan={}'.format(self.batch_size),
-                                                      '--new={}'.format(self.current_batch),
+                                                      '--nsample={}'.format(self.batch_size),
+                                                      '--override={}'.format(self.current_batch),
                                                       logfile=log_file)
 
                 if self.services.wait_task(task_wait):
@@ -102,7 +102,7 @@ class massive_parallel_runner_init(Component):
                                                       self.SAMPLE_EXE,
                                                       '--input={}'.format(self.inscan_config_file),
                                                       '--output=inscan',
-                                                      '--nscan={}'.format(self.batch_size),
+                                                      '--nsample={}'.format(self.batch_size),
                                                       logfile=log_file)
 
                 if self.services.wait_task(task_wait):
